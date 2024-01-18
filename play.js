0a1415870cc75326114d55ddea0bae84fb595456
Jeff Dobson <jbdobson11@gmail.com>
2024-01-17 17:02:43 -0800

    trying to refactor client and play files for exporting

--- play.js
+++ play.js
@@ -1,21 +1,22 @@
-const net = require("net");
+// const net = require("net");
+const connect = requires('./connect');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: 50541
+//   });
 
-  conn.on("data", (data) => {
-    console.log('data: ', data);
-  });
+//   conn.on("data", (data) => {
+//     console.log('data: ', data);
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
