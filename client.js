cf2d294d1e12b350421d7c4c34ac06086e5835b2
Jdob11 <jbdobson11@gmail.com>
2024-02-19 13:42:30 -0800

    refactored user inputs to seperate file

--- client.js
+++ client.js
@@ -30,23 +30,6 @@ const connect = function () {
   return conn;
 };
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
 module.exports = {
   connect,
-  setupInput
 };
