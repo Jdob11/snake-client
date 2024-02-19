688ac45ee19767496fcd5112bffdbc2a4e2ed013
Jdob11 <jbdobson11@gmail.com>
2024-02-19 13:37:05 -0800

    added stdin object and ctrl +c exoit command

--- client.js
+++ client.js
@@ -23,9 +23,30 @@ const connect = function () {
     conn.write("Name: EGG");
   });
 
+  conn.on("connect", () => {
+    conn.write("Move: up");
+  });
+
   return conn;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
 };
 
 module.exports = {
   connect,
+  setupInput
 };
